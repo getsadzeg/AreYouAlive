e3082d1673d3e9179026574d0bbac0607952eb3b
getsadzeg <getsadzeguram@gmail.com>
2015-09-06 23:14:15 +0400

    raising unit; with bugs yet

--- app/src/main/java/none/areyoualive/MainActivity.java
+++ app/src/main/java/none/areyoualive/MainActivity.java
@@ -25,11 +25,11 @@ public class MainActivity extends ActionBarActivity {
     GPSTracker gpsTracker;
 
     SoldierServices ss;
-    public static String[] names = new String[12];
-    public static String[] statuses = new String[12];
-    public static String[] messages = new String[12];
-    public static double[] longitudes = new double[12];
-    public static double[] latitudes = new double[12];
+    public static String[] names = new String[8];
+    public static String[] statuses = new String[8];
+    public static String[] messages = new String[8];
+    public static double[] longitudes = new double[8];
+    public static double[] latitudes = new double[8];
     public static String userstring;
 
     @Override
@@ -62,17 +62,79 @@ public class MainActivity extends ActionBarActivity {
             public void success(SoldierResponse soldierResponse, Response response) {
                 System.out.println("soldierResponse.soldiers.size() = " + soldierResponse.soldiers.size());
                 for (int i = 0; i < soldierResponse.soldiers.size(); i++) {
-                    names[0] = soldierResponse.soldiers.get(0).Name;
+                    if(soldierResponse.soldiers.size() == 1) {
+                        names[0] = soldierResponse.soldiers.get(0).Name;
+                        break;
+                    }
+                    else if(soldierResponse.soldiers.size() == 2) {
+                        names[0] = soldierResponse.soldiers.get(0).Name;
+                        names[1] = soldierResponse.soldiers.get(1).Name;
+                        break;
+                    }
+                    else if(soldierResponse.soldiers.size() == 3) {
+                        names[0] = soldierResponse.soldiers.get(0).Name;
+                        names[1] = soldierResponse.soldiers.get(1).Name;
+                        names[2] = soldierResponse.soldiers.get(2).Name;
+                        break;
+                    }
+                    else if(soldierResponse.soldiers.size() == 4) {
+                        names[0] = soldierResponse.soldiers.get(0).Name;
+                        names[1] = soldierResponse.soldiers.get(1).Name;
+                        names[2] = soldierResponse.soldiers.get(2).Name;
+                        names[3] = soldierResponse.soldiers.get(3).Name;
+                        break;
+                    }
+                    else if(soldierResponse.soldiers.size() == 5) {
+                        names[0] = soldierResponse.soldiers.get(0).Name;
+                        names[1] = soldierResponse.soldiers.get(1).Name;
+                        names[2] = soldierResponse.soldiers.get(2).Name;
+                        names[3] = soldierResponse.soldiers.get(3).Name;
+                        names[4] = soldierResponse.soldiers.get(4).Name;
+                        break;
+                    }
+                    else if(soldierResponse.soldiers.size() == 6) {
+                        names[0] = soldierResponse.soldiers.get(0).Name;
+                        names[1] = soldierResponse.soldiers.get(1).Name;
+                        names[2] = soldierResponse.soldiers.get(2).Name;
+                        names[3] = soldierResponse.soldiers.get(3).Name;
+                        names[4] = soldierResponse.soldiers.get(4).Name;
+                        names[5] = soldierResponse.soldiers.get(5).Name;
+                    }
+                    //names[5] = soldierResponse.soldiers.get(5).Name;
+                    //names[6] = soldierResponse.soldiers.get(6).Name;
+                    //names[7] = soldierResponse.soldiers.get(7).Name;
                     statuses[0] = soldierResponse.soldiers.get(0).Status;
-                    messages[0] = soldierResponse.soldiers.get(0).Message;
-                    longitudes[0] = gpsTracker.getLongitude();
-                    latitudes[0] = gpsTracker.getLatitude();
-                    names[1] = soldierResponse.soldiers.get(1).Name;
                     statuses[1] = soldierResponse.soldiers.get(1).Status;
+                    statuses[2] = soldierResponse.soldiers.get(2).Status;
+                    statuses[3] = soldierResponse.soldiers.get(3).Status;
+                    statuses[4] = soldierResponse.soldiers.get(4).Status;
+                    //statuses[5] = soldierResponse.soldiers.get(5).Status;
+                    //statuses[6] = soldierResponse.soldiers.get(6).Status;
+                    //statuses[7] = soldierResponse.soldiers.get(7).Status;
+                    messages[0] = soldierResponse.soldiers.get(0).Message;
                     messages[1] = soldierResponse.soldiers.get(1).Message;
+                    messages[2] = soldierResponse.soldiers.get(2).Message;
+                    messages[3] = soldierResponse.soldiers.get(3).Message;
+                    messages[4] = soldierResponse.soldiers.get(4).Message;
+                    //messages[5] = soldierResponse.soldiers.get(5).Message;
+                    //messages[6] = soldierResponse.soldiers.get(6).Message;
+                    //messages[7] = soldierResponse.soldiers.get(7).Message;
+                    longitudes[0] = gpsTracker.getLongitude();
                     longitudes[1] = gpsTracker.getLongitude();
+                    longitudes[2] = gpsTracker.getLongitude();
+                    longitudes[3] = gpsTracker.getLongitude();
+                    longitudes[4] = gpsTracker.getLongitude();
+                    //longitudes[5] = gpsTracker.getLongitude();
+                    //longitudes[6] = gpsTracker.getLongitude();
+                    //longitudes[7] = gpsTracker.getLongitude();
+                    latitudes[0] = gpsTracker.getLatitude();
                     latitudes[1] = gpsTracker.getLatitude();
-                    //System.out.println("soldierResponse output is " + soldierResponse.soldiers.get(i).Name);
+                    latitudes[2] = gpsTracker.getLatitude();
+                    latitudes[3] = gpsTracker.getLatitude();
+                    latitudes[4] = gpsTracker.getLatitude();
+                    //latitudes[5] = gpsTracker.getLatitude();
+                    //latitudes[6] = gpsTracker.getLatitude();
+                    //latitudes[7] = gpsTracker.getLatitude();
                 }
 
             }
@@ -133,12 +195,24 @@ public class MainActivity extends ActionBarActivity {
     public void buttonflatOnClick(View v) {
         username = (EditText) findViewById(R.id.editText1);
         password = (EditText) findViewById(R.id.editText2);
-        //password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         userstring = username.getText().toString();
         if (password.getText().toString().equals("lala") && username.getText().toString().equals(names[0]) || username.getText().toString().equals(names[1])) {
             startActivity(new Intent(MainActivity.this, Activity2.class));
             finish();
-        } else {
+        }
+        else if(password.getText().toString().equals("lala") && username.getText().toString().equals(names[2]) || username.getText().toString().equals(names[3])) {
+            startActivity(new Intent(MainActivity.this, Activity2.class));
+            finish();
+        }
+        else if(password.getText().toString().equals("lala") && username.getText().toString().equals(names[4]) || username.getText().toString().equals(names[5])) {
+            startActivity(new Intent(MainActivity.this, Activity2.class));
+            finish();
+        }
+        else if(password.getText().toString().equals("lala") && username.getText().toString().equals(names[6]) || username.getText().toString().equals(names[7])) {
+            startActivity(new Intent(MainActivity.this, Activity2.class));
+            finish();
+        }
+        else {
             Dialog dialog = new Dialog(this, "SOS", "You're trying to access our data. Go to hell. Regards.");
             dialog.show();
         }
